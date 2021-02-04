02315240f0209eb24e169bb660368f335037f912
anastasia <anastasia.naumova08@yandex.ru>
2021-02-04 11:23:37 +0300

    Добавление функции 'delete_comment' в SQL-файлах. Изменение функции 'split_sql'. Добавление функции 'main'. Довавление вызова функции, 'delete_comment', в функцию 'validate_syntax'.

--- validate.py
+++ validate.py
@@ -19,31 +19,49 @@ def read_file(path_sql):
 
 
 def split_sql(sql_script):
-    line_comment = False
-    multiline_comment = False
     flg_text = False
     i = 0
     j = 0
     result = []
     while i < len(sql_script):
-        if sql_script[i:i + 2] == '--':
-            line_comment = True
-        if sql_script[i] == '\n':
-            line_comment = False
-        if sql_script[i:i + 2] == '/*':
-            multiline_comment = True
-        if sql_script[i:i + 2] == '*/':
-            multiline_comment = False
         if sql_script[i] == "'":
             flg_text = not flg_text
-
-        if sql_script[i] == ';' and not line_comment and not multiline_comment and not flg_text:
+        if sql_script[i] == ';' and not flg_text:
             result.append(sql_script[j:i + 1])
             j = i + 1
         i += 1
-    if len(sql_script) != 0 and sql_script[-1] != ';' and not line_comment and not multiline_comment and not flg_text:
+    if len(sql_script) != 0 and sql_script[-1] != ';' and not flg_text:
         result.append(sql_script[j:i + 1].strip())
     return result
+
+
+def delete_comment(commands):
+    line_comment = False
+    multiline_comment = False
+    flg_text = False
+    i = 0
+    j = 0
+    while i < len(commands):
+        if commands[i:i+2] == '--' and not multiline_comment and not flg_text:
+            j = i
+            line_comment = True
+        if commands[i] == '\n' and line_comment:
+            commands = commands[:j]+commands[i+1:]
+            line_comment = False
+            i = j
+        if commands[i:i+2] == '/*' and not line_comment and not flg_text:
+            j = i
+            multiline_comment = True
+        if commands[i:i+2] == '*/' and multiline_comment:
+            commands = commands[:j]+commands[i+2:]
+            multiline_comment = False
+            i = j
+        if commands[i] == "'" and not line_comment and not multiline_comment:
+            flg_text = not flg_text
+        i += 1
+    if commands[-1] != '\n' and line_comment and not multiline_comment and not flg_text:
+        commands = commands[:j]+commands[i:]
+    return commands
 
 
 def validate_syntax(sql_command):
@@ -64,7 +82,8 @@ def validate_package(path):
         syntax_error = []
         script_file = read_file(path_sql)
         if len(script_file) != 0:
-            for sql_command in split_sql(script_file):
+            clean_commands = delete_comment(script_file)
+            for sql_command in split_sql(clean_commands):
                 if validate_syntax(sql_command) is not None:
                     syntax_error.append(validate_syntax(sql_command))
             result[path_sql] = syntax_error
@@ -85,3 +104,15 @@ def error_format(files_and_errors):
                 result.append(x)
             result.append('-'*50 + '\n')
     return result
+
+
+if __name__ == '__main__':
+    path_dir = 'D:/Project/test'
+    with open(path_dir + '/error.txt', 'w') as new_file:
+        new_file.flush()
+
+    result_validate = validate_package(path_dir)
+    result_txt = error_format(result_validate)
+
+    with open(path_dir + '/error.txt', 'w') as f:
+        f.write(' '.join(result_txt))
